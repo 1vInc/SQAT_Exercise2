bca0c8e9b2cced3746142fc8f132456f188bc950
somename <someemail>
2016-10-19 14:42:09 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -8,8 +8,7 @@ public class TestPlanetExplorer {
 	public void test_startingPosition_x0y0N() {
 		PlanetExplorer explorer = new PlanetExplorer(0,0,null);
 		
-		assertEquals("(0,0,E)", executeCommand("r"));
-		fail("Not yet implemented");
+		assertEquals("(0,0,E)", explorer.executeCommand("r"));
 	}
 	
 	
