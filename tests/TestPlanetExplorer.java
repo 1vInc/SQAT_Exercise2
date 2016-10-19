3ef3858ebe0f6364dbeb1558908568c4d93a2a7a
somename <someemail>
2016-10-19 15:00:43 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -3,13 +3,19 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestPlanetExplorer {
+	public TestPlanetExplorer () {
+		PlanetExplorer explorer = new PlanetExplorer(0,0,null);
+	}
 
 	@Test
 	public void test_startingPosition_x0y0N() throws PlanetExplorerException {
-		PlanetExplorer explorer = new PlanetExplorer(0,0,null);
+		
 		
 		assertEquals("(0,0,E)", explorer.executeCommand("r"));
 	}
 	
-	
+	@Test
+	public void test_leftTurnFromStartingPositionWorks() throws PlanetExplorerException {
+		
+	}
 }
